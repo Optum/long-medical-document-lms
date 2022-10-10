281899824d3940cfadf114f6962d66d6f0254227
jstremme <joel_stremmel@optum.com>
2022-10-10 08:37:11 -0500

    handle all pad block

--- explain/msp.py
+++ explain/msp.py
@@ -229,7 +229,7 @@ def post_process_and_save_msp_results(
             final_df["label"] = label
 
             # Remove padding
-            all_pad_block = tokenizer.pad_token * k
+            all_pad_block = (tokenizer.pad_token + " ") * (k - 1) + tokenizer.pad_token
             final_df = final_df[final_df["text_block"] != all_pad_block]
 
             # Build the top M dataframe based on the masking procedure and add p values
