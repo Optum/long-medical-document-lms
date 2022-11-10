489217bdfef66753e8281fae5c72693667abad1d
jstremme <joel_stremmel@optum.com>
2022-11-10 13:44:24 -0600

    update ft

--- models/ft.py
+++ models/ft.py
@@ -27,7 +27,7 @@ from transformers import (
     EarlyStoppingCallback,
 )
 from utils import check_empty_count_gpus, create_current_run, create_log_dir
-from metrics import compute_training_metrics, BootstrapMultiLabelMetrics
+from metrics import BootstrapMultiLabelMetrics
 
 
 def main():
@@ -47,19 +47,22 @@ def main():
 
     # Check, Empty, and Count GPUs
     check_empty_count_gpus(logger=logger)
+    
+    # Only create a run directory if training a new model
+    if PARAMS["train"]:
 
-    # Create Run Directory
-    current_run_dir = create_current_run(
-        save_path=PARAMS["output_path"], params=PARAMS, logger=logger
-    )
-    logger.info(f"Created run directory: {current_run_dir}.")
+        # Create Run Directory
+        current_run_dir = create_current_run(
+            save_path=PARAMS["output_path"], params=PARAMS, logger=logger
+        )
+        logger.info(f"Created run directory: {current_run_dir}.")
 
-    # Create logging dir
-    logging_dir = create_log_dir(current_run_dir)
+        # Create logging dir
+        logging_dir = create_log_dir(current_run_dir)
 
-    # Set Run Name
-    run_name = current_run_dir.split("/")[-1]
-    logger.info(f"Starting run {run_name}...")
+        # Set Run Name
+        run_name = current_run_dir.split("/")[-1]
+        logger.info(f"Starting run {run_name}...")
 
     # Load LM tokenizer
     tokenizer = AutoTokenizer.from_pretrained(PARAMS["lm_path"])
@@ -129,104 +132,111 @@ def main():
         )
         .with_format("torch")
     )
+    
+    # Define everything we train a new model
+    # Or to continue training a model
+    # Then, train it
+    if PARAMS["train"]:
 
-    # Define problem type
-    # PyTorch expects multi-hot labels where each element is a float
-    # For multi-label classification with binary_cross_entropy_with_logits loss
-    if PARAMS["class_strategy"] == "binary":
-        problem_type = "single_label_classification"
-    elif PARAMS["class_strategy"] == "multi_label":
-        problem_type = "multi_label_classification"
+        # Define problem type
+        # PyTorch expects multi-hot labels where each element is a float
+        # For multi-label classification with binary_cross_entropy_with_logits loss
+        if PARAMS["class_strategy"] == "binary":
+            problem_type = "single_label_classification"
+        elif PARAMS["class_strategy"] == "multi_label":
+            problem_type = "multi_label_classification"
 
-    # Create sequence classifier from pretrained model
-    model = AutoModelForSequenceClassification.from_pretrained(
-        PARAMS["lm_path"],
-        num_labels=PARAMS["num_labels"],
-        return_dict=True,
-        problem_type=problem_type,
-    )
+        # Create sequence classifier from pretrained model
+        model = AutoModelForSequenceClassification.from_pretrained(
+            PARAMS["lm_path"],
+            num_labels=PARAMS["num_labels"],
+            return_dict=True,
+            problem_type=problem_type,
+        )
 
-    # Define early stopping callback
-    early_stopping = EarlyStoppingCallback(
-        early_stopping_patience=PARAMS["early_stopping_patience"]
-    )
+        # Define early stopping callback
+        early_stopping = EarlyStoppingCallback(
+            early_stopping_patience=PARAMS["early_stopping_patience"]
+        )
 
-    # Define training arguments
-    training_args = TrainingArguments(
-        output_dir=current_run_dir,
-        max_steps=PARAMS["max_steps"],
-        per_device_train_batch_size=PARAMS["per_device_train_batch_size"],
-        gradient_accumulation_steps=PARAMS["accumulation_steps"],
-        per_device_eval_batch_size=PARAMS["per_device_eval_batch_size"],
-        metric_for_best_model=PARAMS["metric_for_best_model"],
-        eval_steps=PARAMS["eval_steps"],
-        save_steps=PARAMS["save_steps"],
-        logging_steps=PARAMS["logging_steps"],
-        evaluation_strategy=PARAMS["evaluation_strategy"],
-        save_strategy=PARAMS["save_strategy"],
-        lr_scheduler_type=PARAMS["lr_scheduler_type"],
-        warmup_steps=PARAMS["warmup_steps"],
-        weight_decay=PARAMS["weight_decay"],
-        learning_rate=PARAMS["learning_rate"],
-        fp16=PARAMS["fp16"],
-        fp16_full_eval=PARAMS["fp16_eval"],
-        seed=PARAMS["seed"],
-        adam_beta1=PARAMS["adam_beta1"],
-        adam_beta2=PARAMS["adam_beta2"],
-        adam_epsilon=PARAMS["adam_epsilon"],
-        sharded_ddp=PARAMS["sharded_ddp"],
-        optim="adamw_torch",
-        disable_tqdm=PARAMS["disable_tqdm"],
-        load_best_model_at_end=True,
-        logging_dir=logging_dir,
-        dataloader_num_workers=PARAMS["dataloader_num_workers"],
-        run_name=run_name,
-        eval_accumulation_steps=PARAMS["eval_accumulation_steps"],
-        gradient_checkpointing=PARAMS["gradient_checkpointing"],
-    )
+        # Define training arguments
+        training_args = TrainingArguments(
+            output_dir=current_run_dir,
+            max_steps=PARAMS["max_steps"],
+            per_device_train_batch_size=PARAMS["per_device_train_batch_size"],
+            gradient_accumulation_steps=PARAMS["accumulation_steps"],
+            per_device_eval_batch_size=PARAMS["per_device_eval_batch_size"],
+            metric_for_best_model=PARAMS["metric_for_best_model"],
+            eval_steps=PARAMS["eval_steps"],
+            save_steps=PARAMS["save_steps"],
+            logging_steps=PARAMS["logging_steps"],
+            evaluation_strategy=PARAMS["evaluation_strategy"],
+            save_strategy=PARAMS["save_strategy"],
+            lr_scheduler_type=PARAMS["lr_scheduler_type"],
+            warmup_steps=PARAMS["warmup_steps"],
+            weight_decay=PARAMS["weight_decay"],
+            learning_rate=PARAMS["learning_rate"],
+            fp16=PARAMS["fp16"],
+            fp16_full_eval=PARAMS["fp16_eval"],
+            seed=PARAMS["seed"],
+            adam_beta1=PARAMS["adam_beta1"],
+            adam_beta2=PARAMS["adam_beta2"],
+            adam_epsilon=PARAMS["adam_epsilon"],
+            sharded_ddp=PARAMS["sharded_ddp"],
+            optim="adamw_torch",
+            disable_tqdm=PARAMS["disable_tqdm"],
+            load_best_model_at_end=True,
+            prediction_loss_only=True, # Added
+            logging_dir=logging_dir,
+            dataloader_num_workers=PARAMS["dataloader_num_workers"],
+            run_name=run_name,
+            eval_accumulation_steps=PARAMS["eval_accumulation_steps"],
+            gradient_checkpointing=PARAMS["gradient_checkpointing"],
+        )
 
-    # Define trainer
-    trainer = Trainer(
-        model=model,
-        args=training_args,
-        compute_metrics=partial(
-            compute_training_metrics, class_strategy=PARAMS["class_strategy"]
-        ),
-        train_dataset=dataset["train"],
-        eval_dataset=dataset["val"],
-        callbacks=[early_stopping],
-    )
+        # Define trainer
+        trainer = Trainer(
+            model=model,
+            args=training_args,
+            train_dataset=dataset["train"],
+            eval_dataset=dataset["val"],
+            callbacks=[early_stopping]
+        )
 
-    # Start training timer
-    start_time = time.time()
+        # Start training timer
+        start_time = time.time()
 
-    # Start from model provided above and new training parameters defined above
-    if not PARAMS["resume_training"]:
+        # Start from model provided above and new training parameters defined above
+        if not PARAMS["resume_training"]:
 
-        # Train model
-        trainer.train()
+            # Train model
+            trainer.train()
 
-    # Resume using model and training parameters defined in checkpoint
+        # Resume using model and training parameters defined in checkpoint
+        else:
+
+            # Train model
+            trainer.train(PARAMS["checkpoint"])
+
+        # Log training time
+        end_time = time.time()
+        execution_time_hours = round((end_time - start_time) / 3600.0, 2)
+        logger.info(f"Training took {execution_time_hours} hours.")
+
+        # Save best model
+        trainer.model.save_pretrained(
+            os.path.join(current_run_dir, f'{PARAMS["model_name"]}_model')
+        )
+        tokenizer.save_pretrained(
+            os.path.join(current_run_dir, f'{PARAMS["model_name"]}_tokenizer')
+        )
+    
+    # Load a model for testing
     else:
-
-        # Train model
-        trainer.train(PARAMS["checkpoint"])
-
-    # Log training time
-    end_time = time.time()
-    execution_time_hours = round((end_time - start_time) / 3600.0, 2)
-    logger.info(f"Training took {execution_time_hours} hours.")
-
-    # Save best model
-    trainer.model.save_pretrained(
-        os.path.join(current_run_dir, f'{PARAMS["model_name"]}_model')
-    )
-    tokenizer.save_pretrained(
-        os.path.join(current_run_dir, f'{PARAMS["model_name"]}_tokenizer')
-    )
-
-    # Evaluate model
-    trainer.evaluate()
+        
+        # Define model and trainer for testing only
+        model = AutoModelForSequenceClassification.from_pretrained(PARAMS["test_model_checkpoint"])
+        trainer = Trainer(model=model)
 
     # Predict on test data
     output = trainer.predict(dataset["test"])
@@ -234,26 +244,33 @@ def main():
 
     # Apply appropriate probability transformation
     if PARAMS["class_strategy"] == "multi_label":
-        probs = torch.sigmoid(output.predictions)
+        probs = torch.sigmoid(torch.tensor(output.predictions))
     elif PARAMS["class_strategy"] == "multi_class":
-        probs = torch.nn.functional.softmax(output.predictions)
+        probs = torch.nn.functional.softmax(torch.tensor(output.predictions))
     else:
         raise ValueError(
-            f"Expected class_strategy to be one of ['multi_label', 'multi_class'] but got {class_strategy}."
+            f"Expected class_strategy to be one of ['multi_label', 'multi_class'] but got {PARAMS["class_strategy"]}."
         )
 
     # Compute final performance
     evaluator = BootstrapMultiLabelMetrics(labels=labels, preds=probs)
     metrics_dict = evaluator.get_all_bootstrapped_metrics_as_dict(n_bootstrap=1000)
 
-    # Save metrics
-    with open(os.path.join(current_run_dir, "metrics.json"), "w") as f:
-        json.dump(metrics_dict, f)
+    # Save metrics to the training run directory if we're training
+    if PARAMS["train"]:
+        
+        # Save metrics to the training directory
+        with open(os.path.join(current_run_dir, "metrics.json"), "w") as f:
+            json.dump(metrics_dict, f)
+    
+    # Save metrics to the root directory
+    with open("./metrics.json", "w") as f:
+            json.dump(metrics_dict, f)
 
     # Log metrics
     logger.info(metrics_dict)
 
-
+    
 if __name__ == "__main__":
 
     main()
